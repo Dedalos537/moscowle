65e0dc247e72a20f11e4c8110ff3cbc15787e1aa
Op614246 <op614246@gmail.com>
2026-04-19 20:17:59 -0500

    Creación del Edusync

--- fix_scope.py
+++
@@ -1,9 +0,0 @@
-import re
-with open('app/services/receipt_generator.py', 'r') as f:
-    text = f.read()
-
-text = text.replace("    try:\n        logo_path = os.path.join(", "    logo_path = ''\n    try:\n        logo_path = os.path.join(")
-
-with open('app/services/receipt_generator.py', 'w') as f:
-    f.write(text)
-print("scope fixed")
