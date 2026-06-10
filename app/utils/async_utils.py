7de1af240d8546b6e11f40947a44e498d7a9093a
Dedalos537 <diegocenteno537@gmail.com>
2026-06-09 23:40:00 -0500

    refactor: remove async_utils.py (no remaining callers)

--- app/utils/async_utils.py
+++
@@ -1,7 +0,0 @@
-from app.dao.db_async import get_async_db
-from app.dao.base import BaseDAO
-from datetime import datetime
-
-async def get_dao(dao_class, *args, **kwargs):
-    async with get_async_db() as session:
-        yield dao_class(session)
